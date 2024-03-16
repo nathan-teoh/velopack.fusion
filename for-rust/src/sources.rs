0e0e4dcc06d40f2ad22170536ffe0ca5a2ff856e
nathan <junyuteoh+freshairgit@proton.me>
2024-03-16 15:16:53 -0700

    initial implementation and test

--- for-rust/src/sources.rs
+++ for-rust/src/sources.rs
@@ -1,4 +1,5 @@
 use anyhow::Result;
+use url::Url;
 use std::path::{Path, PathBuf};
 
 use crate::*;
@@ -97,4 +98,77 @@ impl UpdateSource for FileSource {
         progress(100);
         Ok(())
     }
+}
+#[derive(Clone)]
+pub struct GithubUpdateSource {
+    url: String,
+}
+
+impl GithubUpdateSource {
+    /// Create a new GithubUpdateSource with the specified base URL.
+    pub fn new(url: &str) -> Self {
+        GithubUpdateSource { 
+            url: url.to_owned() 
+        }
+    }
+
+    fn get_api_base_url(&self) -> Result<String> {
+        let base_url: String;
+
+        //check for valid URL
+        match Url::parse(&self.url) {
+            Ok(url) => {
+                //handle 2 cases:
+                //if github.com url
+                if url.host_str() == Some("github.com") {
+                    base_url = String::from("https://api.github.com/");
+                } else {
+                    //if not github.com url, it's probably an enterprise server
+                    base_url = format!("{}://{}/api/v3/", url.scheme(), url.host_str().unwrap());
+                }
+
+                return Ok(base_url);
+            }
+            Err(err) => {
+                panic!("Invalid URL. We should never be here.");
+            }
+        }
+    }
+}
+
+impl UpdateSource for GithubUpdateSource {
+    fn get_release_feed(&self, channel: &str, app: &manifest::Manifest) -> Result<VelopackAssetFeed> {
+        let per_page = 10;
+        let page = 1;
+        let releases_path = format!("repos/{}/releases?per_page{per_page}&page={page}", self.url);
+        let base_path = self.get_api_base_url()?;
+        let get_releases_uri = format!("{base_path}{releases_path}");
+        let response = download::download_url_as_string(&get_releases_uri)?;
+        let releases: VelopackAssetFeed = serde_json::from_str(&response)?;
+        Ok(releases)
+    }
+
+    fn download_release_entry<A>(&self, asset: &VelopackAsset, local_file: &str, progress: A) -> Result<()>
+        where A: FnMut(i16)
+    {
+        let path = self.url.trim_end_matches('/').to_owned() + "/";
+        let url = url::Url::parse(&path)?;
+        let asset_url = url.join(&asset.FileName)?;
+
+        info!("About to download from URL '{}' to file '{}'", asset_url, local_file);
+        download::download_url_to_file(asset_url.as_str(), local_file, progress)?;
+        Ok(())
+    }
+}
+
+#[test]
+fn test_get_api_base_url(){
+    let normal_gh_url = "https://github.com/velopack/velopack/";
+    let enterprise_gh_url = "http://internal.github.server.local/";
+
+    let normal_gh_source = GithubUpdateSource::new(normal_gh_url);
+    let enterprise_gh_source = GithubUpdateSource::new(enterprise_gh_url);
+
+    assert_eq!("https://api.github.com/",normal_gh_source.get_api_base_url().unwrap());
+    assert_eq!("http://internal.github.server.local/api/v3/",enterprise_gh_source.get_api_base_url().unwrap());
 }
